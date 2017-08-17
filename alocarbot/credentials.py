75752b04c4873bc6dab6b8270b6b7306a92a2528
Caue Baasch de Souza <cauebaasch@gmail.com>
2017-08-17 00:28:20 -0300

    Fixed config path

--- alocarbot/credentials.py
+++ alocarbot/credentials.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 
-with open(Path.home() / '.config') as f:
+with open(Path.home() / '.config/alocarbot.json') as f:
     config = json.load(f)
 
 USERNAME = config['username']
