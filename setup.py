68e30eace5fc29c5f07649f79fa008843e0343ae
Cauê Baasch de Souza <cauebs@protonmail.com>
2018-02-26 21:19:17 -0300

    Forgot to update requirements

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='alocarbot',
     packages=find_packages(),
-    install_requires=['requests', 'beautifulsoup4', 'robobrowser',
-                      'dataset', 'python-telegram-bot'],
+    install_requires=['cagrex', 'dataset', 'python-telegram-bot'],
     entry_points={
         'console_scripts': [
             'alocarbot = alocarbot.__main__:run',
